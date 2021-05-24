c3ee9f72f9b731e656b3eb2009d3c81d50a0afeb
Vilord <vilordugapev@gmail.com>
2021-05-24 13:33:56 +0900

    Добавил обработку нажатия на район Мирный для теста

--- app/src/main/java/space/rob/map_app/QuizActivity.java
+++ app/src/main/java/space/rob/map_app/QuizActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.richpath.RichPath;
+import com.richpath.RichPathView;
+import com.richpathanimator.RichPathAnimator;
+
 public class QuizActivity extends AppCompatActivity {
+
+    RichPathView richPathView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        richPathView = findViewById(R.id.map);
+        richPathView.setOnPathClickListener(new RichPath.OnPathClickListener() {
+            @Override
+            public void onClick(RichPath richPath) {
+                if (richPath.getName().equals("Mirny")) {
+                    RichPathAnimator.animate(richPath)
+                            .fillColor((255 & 0xff) << 24 | (255 & 0xff) << 16 | (0 & 0xff) << 8 | (0 & 0xff))
+                            .start();
+                }
+            }
+        });
     }
 }
