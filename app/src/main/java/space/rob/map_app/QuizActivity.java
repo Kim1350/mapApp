3ad1580dcf07757479120a56d8ad1e8e7fb70977
Vilord <vilordugapev@gmail.com>
2021-05-24 18:18:02 +0900

    Небольшая доработка QuizActivity.java и activity_quiz.xml

--- app/src/main/java/space/rob/map_app/QuizActivity.java
+++ app/src/main/java/space/rob/map_app/QuizActivity.java
@@ -1,7 +1,5 @@
 package space.rob.map_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -9,12 +7,19 @@ import com.richpath.RichPath;
 import com.richpath.RichPathView;
 import com.richpathanimator.RichPathAnimator;
 import android.app.Activity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import org.w3c.dom.Text;
+
 
 public class QuizActivity extends Activity {
+
+    boolean canClick = true;
 
     RichPathView richPathView;
 
@@ -25,15 +30,15 @@ public class QuizActivity extends Activity {
             "Region_20", "Region_21", "Region_22", "Region_23", "Region_24",
             "Region_25", "Region_26", "Region_27", "Region_28", "Region_29",
             "Region_30", "Region_31", "Region_32", "Region_32", "Region_33",
-            "Region_34"};
+            "Region_34", "Region_35"};
 
-    String[] regions = {"Абыйский", "Алданский", "Аллаиховский", "Амгинский", "Анабарский",
-            "Булунский", "Верхневилюйский", "Верхнеколымский", "Верхоянский", "Вилюйский",
-            "Горный", "Жиганский", "Кобяйский", "Ленский", "Мегино-Кангаласский",
-            "Мирный", "Момский", "Намский", "Нерюнгринский", "Нижеколымский",
-            "Нюрбинский", "Оймяконский", "Оленекский", "Олекминский", "Среднеколымский",
-            "Сунтарский", "Таттинский", "Томпонский", "Усть-Алданский", "Усть-Майский",
-            "Усть-Янский", "Хангаласский", "Чурапчинский", "Эвено-Бытантайский"};
+    String[] regions = {"Абыйский улус", "Алданский улус", "Аллаиховский улус", "Амгинский улус", "Анабарский национальный (долгано-эвенкийский) улус",
+            "Булунский улус", "Верхневилюйский улус", "Верхнеколымский улус", "Верхоянский улус", "Вилюйский улус",
+            "Горный улус", "Жиганский национальный эвенкийский улус", "Кобяйский улус", "Ленский улус", "Мегино-Кангаласский улус",
+            "Мирнинский район", "Момский национальный эвенский улус", "Намский улус", "Нерюнгринский улус", "Нижеколымский улус",
+            "Нюрбинский улус", "Оймяконский улус", "Оленекский эвенкийский национальный улус", "Олекминский улус", "Среднеколымский улус",
+            "Сунтарский улус", "Таттинский улус", "Томпонский улус", "Усть-Алданский улус", "Усть-Майский улус",
+            "Усть-Янский улус", "Хангаласский улус", "Чурапчинский улус", "Эвено-Бытантайский национальный улус", "город Якутск"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +49,21 @@ public class QuizActivity extends Activity {
 
         TextView textView = findViewById(R.id.txtNextRegion);
         int randomNumber = (int)((Math.random() * (34 - 0)) + 0);
-        textView.setText(regions[randomNumber]);
+        textView.setText("Найдите " + regions[randomNumber]);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.playAgain);
+        TextView txtPlayAgain = findViewById(R.id.txtPlayAgain);
 
         richPathView = findViewById(R.id.map);
         richPathView.setOnPathClickListener(new RichPath.OnPathClickListener() {
             @Override
             public void onClick(RichPath richPath) {
-                if (richPath.getName().equals(regionNumbers[randomNumber])) {
+                if (richPath.getName().equals(regionNumbers[randomNumber]) && canClick) {
+                    canClick = false;
+                    txtPlayAgain.setText("Поздравляем! Вы нашли " + regions[randomNumber] +"! Хотите сыграть еще раз?");
+                    constraintLayout.setVisibility(View.VISIBLE);
+
+
                     if(richPath.getName().equals("Region_6")) {
                         for(int i = 0; i < 15; i++) {
                             RichPath richPathRegion6 = richPathView.findRichPathByIndex(i);
@@ -63,7 +76,7 @@ public class QuizActivity extends Activity {
                                 .fillColor((255 & 0xff) << 24 | (50 & 0xff) << 16 | (128 & 0xff) << 8 | (50 & 0xff))
                                 .start();
                     }
-                } else if (!richPath.getName().equals(regionNumbers[randomNumber])) {
+                } else if (!richPath.getName().equals(regionNumbers[randomNumber]) && canClick) {
                     if(richPath.getName().equals("Region_6")) {
                         for(int i = 0; i < 15; i++) {
                             RichPath richPathRegion6 = richPathView.findRichPathByIndex(i);
@@ -80,6 +93,18 @@ public class QuizActivity extends Activity {
             }
         });
     }
+
+    public void PlayAgain(View view) {
+        finish();
+        startActivity(getIntent());
+    }
+
+    public void EndGame(View view) {
+        Intent intent = new Intent(QuizActivity.this, Menu.class);
+        startActivity(intent);
+        finish();
+    }
+
     //системная кнопка назад
     @Override
     public void onBackPressed()
